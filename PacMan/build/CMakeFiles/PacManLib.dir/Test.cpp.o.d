af5b6a8ddb88cb38b0694c2e8a8df3ba04ac300c
flurin-b <129754174+flurin-b@users.noreply.github.com>
2023-11-28 10:28:27 +0100

    Delete PacMan/build directory
    The build output is now saves as an artifact, therefore this Folder is no longer needed.

--- PacMan/build/CMakeFiles/PacManLib.dir/Test.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/PacManLib.dir/Test.cpp.o: \
- /home/runner/work/PMSwEng/PMSwEng/PacMan/Test.cpp \
- /usr/include/stdc-predef.h \
- /home/runner/work/PMSwEng/PMSwEng/PacMan/Test.h
--- PacMan/build/Tests/CMakeFiles/Test.dir/Test_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-Tests/CMakeFiles/Test.dir/Test_autogen/mocs_compilation.cpp.o: \
- /home/runner/work/PMSwEng/PMSwEng/PacMan/build/Tests/Test_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
